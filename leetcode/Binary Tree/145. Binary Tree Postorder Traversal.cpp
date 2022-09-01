f629f218d1a1835fa6c264f649c56efaf329073c
fabienzz <58698376+fabienzz@users.noreply.github.com>
2022-09-01 12:58:50 -0400

    Update 145. Binary Tree Postorder Traversal.cpp

--- leetcode/Binary Tree/145. Binary Tree Postorder Traversal.cpp
+++ leetcode/Binary Tree/145. Binary Tree Postorder Traversal.cpp
@@ -26,3 +26,22 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> result;
+        if (root == NULL) return result;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            result.push_back(node->val);
+            if (node->left) st.push(node->left); // 相对于前序遍历，这更改一下入栈顺序 （空节点不入栈）
+            if (node->right) st.push(node->right); // 空节点不入栈
+        }
+        reverse(result.begin(), result.end()); // 将结果反转之后就是左右中的顺序了
+        return result;
+    }
+};
