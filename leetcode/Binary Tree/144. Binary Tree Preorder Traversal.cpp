afab9945e565328293a26e6d2e601cbd7a3863ac
fabienzz <58698376+fabienzz@users.noreply.github.com>
2022-08-31 23:29:58 -0400

    Update 144. Binary Tree Preorder Traversal.cpp

--- leetcode/Binary Tree/144. Binary Tree Preorder Traversal.cpp
+++ leetcode/Binary Tree/144. Binary Tree Preorder Traversal.cpp
@@ -29,3 +29,22 @@ public:
             return;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> result;
+        if (root == NULL) return result;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();                       // 中
+            st.pop();
+            result.push_back(node->val);
+            if (node->right) st.push(node->right);           // 右（空节点不入栈）
+            if (node->left) st.push(node->left);             // 左（空节点不入栈）
+        }
+        return result;
+    }
+};
