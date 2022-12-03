1bec8a5cc67cb79a7618dab444db18ac13fb597a
fabienzz <58698376+fabienzz@users.noreply.github.com>
2022-12-02 19:35:43 -0500

    Update 1. Two Sum.cpp

--- leetcode/Hash Table/1. Two Sum.cpp
+++ leetcode/Hash Table/1. Two Sum.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> m;
         vector<int> ret;
-        for(int i =0;i<nums.size();i++)
+        for(int i =0;i<nums.size();i++) // map each value to its index
             m[nums[i]] = i;
         
         for(int i =0;i<nums.size();i++){
             int rem = target-nums[i];
-            if(m.find(rem)!=m.end()&&m[rem]!=i){
+            if(m.find(rem)!=m.end()&&m[rem]!=i){ // if the remainder is not current value (cant use the same element twice)
                 ret.push_back(m[rem]);
                 ret.push_back(i);
                 break;
